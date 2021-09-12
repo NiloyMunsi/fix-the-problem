c2d31e24432b0aabcc06d5a2546de057b51ce083
NiloyMunsi <niloymunsi717@gmail.com>
2021-09-13 03:17:23 +0600

    continue

--- js/app.js
+++ js/app.js
@@ -20,8 +20,10 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <p>Rate: ${product.rating.rate} <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i></p>
+      <p>Total rateing person: ${product.rating.count}</p>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn buy-btn">add to cart</button>
+      <button id="details-btn" class="btn details-btn">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -33,18 +35,20 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
   return converted;
+
 };
 
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
-  const convertPrice = value.toFixed(2);
+  const convertPrice = (value);
   const total = convertedOldPrice + convertPrice;
   document.getElementById(id).innerText = Math.round(total);
 };
